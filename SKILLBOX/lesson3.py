0fd48fca6d1e1b4eadb37e2e8e43ebc6e2615274
Mikhail Lomakin <nardcore@yandex.ru>
2022-04-12 13:36:32 +0300

    added 2

---
+++ SKILLBOX/lesson3.py
@@ -0,0 +1,72 @@
+while True:
+	znak = input('Выберите операцию: ')
+	print('Привет, я калькулотор')
+	if znak not in ['-','*','+','/']:
+		print('Ошибка: такой операции не существует. Попробуйте ещё раз.')
+		break
+	elif znak == '+':
+		otvet = 0
+		formula = ''
+		how_match = int(input('Сколько операндов? '))
+		for i in range(1, how_match + 1):
+			digit = int(input(f'Введите {i} операнд: '))
+			otvet += digit
+			if i == how_match:
+				formula += str(digit)
+			else:	
+				formula += str(digit) + ' ' + znak + ' '
+		print(f'{formula} = {otvet}')
+	elif znak == '-':
+		formula = ''
+		how_match = int(input('Сколько операндов? '))
+		for i in range(1, how_match + 1):
+			digit = int(input(f'Введите {i} операнд: '))
+			if i == 1:
+				otvet = digit
+				formula += str(digit) + ' ' + znak + ' '
+			elif i == how_match:
+				otvet -= digit
+				formula += str(digit)
+			else:
+				otvet -= digit	
+				formula += str(digit) + ' ' + znak + ' '
+		print(f'{formula} = {otvet}')
+	elif znak == '*':
+		formula = ''
+		how_match = int(input('Сколько операндов? '))
+		for i in range(1, how_match + 1):
+			digit = int(input(f'Введите {i} операнд: '))	
+			if i == 1:
+				otvet = digit
+				formula += str(digit) + ' ' + znak + ' '
+			elif i == how_match:
+				otvet *= digit
+				formula += str(digit)
+			else:
+				otvet *= digit	
+				formula += str(digit) + ' ' + znak + ' '
+		print(f'{formula} = {otvet}')
+	else:
+		formula = ''
+		how_match = int(input('Сколько операндов? '))
+		for i in range(1, how_match + 1):
+			digit = int(input(f'Введите {i} операнд: '))	
+			if i == 1:
+				otvet = digit
+				formula += str(digit) + ' ' + znak + ' '
+			elif i == how_match:
+				otvet /= digit
+				formula += str(digit)
+			else:
+				otvet /= digit	
+				formula += str(digit) + ' ' + znak + ' '
+		print(f'{formula} = {otvet}')
+			
+
+
+			
+
+
+
+
+
